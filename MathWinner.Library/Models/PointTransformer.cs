77dad0ad7c6eedbdc797ae84f2088399db089e3a
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2025-01-24 18:44:19 +0100

    feat: add Point transform

---
+++ MathWinner.Library/Models/PointTransformer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathWinner.Library.Models
+{
+    public class PointTransformer
+    {
+        public Vector2 Position { get; private set; }
+
+        public PointTransformer(float x, float y)
+        {
+            Position = new Vector2(x, y);
+        }
+
+        public void ReflectOverLineXEquals(float x)
+        {
+            Position = new Vector2(2 * x - Position.X, Position.Y);
+        }
+
+        public void ReflectOverLineYEquals(float y)
+        {
+            Position = new Vector2(Position.X, 2 * y - Position.Y);
+        }
+
+        public void Rotate(float degrees)
+        {
+            float angleRad = MathF.PI * degrees / 180;
+            float cosA = MathF.Cos(angleRad);
+            float sinA = MathF.Sin(angleRad);
+            Position = new Vector2(
+                Position.X * cosA - Position.Y * sinA,
+                Position.X * sinA + Position.Y * cosA
+            );
+        }
+
+        public void ReflectOverOrigin()
+        {
+            Position = new Vector2(-Position.X, -Position.Y);
+        }
+
+        public void Scale(float scaleX, float scaleY)
+        {
+            Position = new Vector2(Position.X * scaleX, Position.Y * scaleY);
+        }
+
+        public void Translate(float dx, float dy)
+        {
+            Position += new Vector2(dx, dy);
+        }
+    }
+}
---
+++ MathWinner.Tests/PracticalTests/PointTransformerTests.cs
@@ -0,0 +1,28 @@
+﻿using MathWinner.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathWinner.Tests.PracticalTests
+{
+    public class PointTransformerTests
+    {
+        [Fact]
+        public void TestTransformations()
+        {
+            PointTransformer point = new PointTransformer(0, -6);
+
+            point.ReflectOverLineXEquals(9);
+            point.Rotate(47);
+            point.ReflectOverOrigin();
+            point.Translate(1, MathF.Exp(MathF.PI));
+
+            double cutX = Math.Truncate(point.Position.X * 100) / 100;
+            Assert.Equal(-15.66, cutX);
+        }
+    }
+
+}
